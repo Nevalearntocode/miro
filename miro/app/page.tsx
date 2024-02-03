27228372017a5270fbb1e5029d9790c3293db0b7
Neva <hmtam110501@gmail.com>
2024-02-03 16:55:03 +0700

    convex/clerk config

---
+++ miro/app/page.tsx
@@ -0,0 +1,11 @@
+import { UserButton } from "@clerk/nextjs";
+
+export default function Home() {
+  return (
+    <div className="flex flex-col gap-y-4">
+      <div>
+        <UserButton afterSignOutUrl="/" />
+      </div>
+    </div>
+  );
+}
---
+++ miro/convex/auth.config.js
@@ -0,0 +1,8 @@
+export default {
+  providers: [
+    {
+      domain: "https://excited-mayfly-20.clerk.accounts.dev",
+      applicationID: "convex",
+    },
+  ],
+};
