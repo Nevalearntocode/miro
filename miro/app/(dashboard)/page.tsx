01c1ae8851afad0b0c8a6946fb04f43c3d9d9a1a
Neva <hmtam110501@gmail.com>
2024-02-03 22:01:01 +0700

    sidebar

--- miro/app/(dashboard)/page.tsx
+++ miro/app/(dashboard)/page.tsx
@@ -3,9 +3,7 @@ import { UserButton } from "@clerk/nextjs";
 export default function Home() {
   return (
     <div className="flex flex-col gap-y-4">
-      <div>
-        <UserButton afterSignOutUrl="/" />
-      </div>
+      <div></div>
     </div>
   );
 }
