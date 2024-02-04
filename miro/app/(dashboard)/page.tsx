eafbaf00462ec9515dda9e5c0a6a6ce141767c82
Neva <hmtam110501@gmail.com>
2024-02-04 21:57:18 +0700

    Boardlist

--- miro/app/(dashboard)/page.tsx
+++ miro/app/(dashboard)/page.tsx
@@ -1,9 +1,27 @@
-import { UserButton } from "@clerk/nextjs";
+"use client";
 
-export default function Home() {
+import { useOrganization } from "@clerk/nextjs";
+import EmptyOrg from "./_components/empty-org";
+import BoardList from "./_components/board-list";
+
+interface HomeProps {
+  searchParams: {
+    search?: string;
+    favorites?: string;
+  };
+}
+
+export default function Home({ searchParams }: HomeProps) {
+  const { organization } = useOrganization();
+
   return (
-    <div className="flex flex-col gap-y-4">
-      <div></div>
+    <div className="flex-1 h-[calc(100%-80px)] p-6">
+      {/* {JSON.stringify(searchParams)} */}
+      {!organization ? (
+        <EmptyOrg />
+      ) : (
+        <BoardList orgId={organization.id} query={searchParams} />
+      )}
     </div>
   );
 }
