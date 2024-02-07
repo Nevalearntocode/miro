3acdd8c7e1e4142394b499c4d60db28cbf9abf8e
Neva <hmtam110501@gmail.com>
2024-02-07 23:48:49 +0700

    Canvas state

--- miro/app/board/[boardId]/_components/canvas.tsx
+++ miro/app/board/[boardId]/_components/canvas.tsx
@@ -4,20 +4,42 @@ import React from "react";
 import Info from "./info";
 import Participants from "./participants";
 import Toolbar from "./toolbar";
-import { useSelf } from "@/liveblocks.config";
+import {
+  useCanRedo,
+  useCanUndo,
+  useHistory,
+  useSelf,
+} from "@/liveblocks.config";
+import { useState } from "react";
+import { CanvasMode, CanvasState } from "@/types/canvas";
 
 interface CanvasProps {
   boardId: string;
 }
 
 const Canvas = ({ boardId }: CanvasProps) => {
+  const [canvasState, setCanvasState] = useState<CanvasState>({
+    mode: CanvasMode.None,
+  });
+
+  const history = useHistory();
+  const canUndo = useCanUndo();
+  const canRedo = useCanRedo();
+
   const info = useSelf((me) => me.info);
 
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
       <Participants />
-      <Toolbar />
+      <Toolbar
+        canvasState={canvasState}
+        setCanvasState={setCanvasState}
+        canRedo={canRedo}
+        canUndo={canUndo}
+        undo={history.undo}
+        redo={history.redo}
+      />
     </main>
   );
 };
