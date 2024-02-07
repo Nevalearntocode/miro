092eb1547cafd5fb342789ab873782f31183d7de
Neva <hmtam110501@gmail.com>
2024-02-07 23:00:29 +0700

    room participants

--- miro/app/board/[boardId]/_components/canvas.tsx
+++ miro/app/board/[boardId]/_components/canvas.tsx
@@ -13,8 +13,6 @@ interface CanvasProps {
 const Canvas = ({ boardId }: CanvasProps) => {
   const info = useSelf((me) => me.info);
 
-  console.log(info);
-
   return (
     <main className="h-full w-full relative bg-neutral-100 touch-none">
       <Info boardId={boardId} />
--- miro/app/board/[boardId]/_components/participants.tsx
+++ miro/app/board/[boardId]/_components/participants.tsx
@@ -1,10 +1,53 @@
+"use client";
+
 import React from "react";
+import UserAvatar from "./user-avatar";
+import { useOthers, useSelf } from "@/liveblocks.config";
+import { connectionIdToColor } from "@/lib/utils";
+
+const MAX_SHOWN_USER = 2;
 
 const Participants = () => {
+  const users = useOthers();
+  const currentUser = useSelf();
+  const hasMoreUsers = users.length > MAX_SHOWN_USER;
+
   return (
     <div className="absolute h-12 top-2 right-2 bg-white rounded-md p-3 flex items-center shadow-md">
-      {/* List of user */}
-      List of user
+      <div className="flex gap-x-2">
+        {users.slice(0, MAX_SHOWN_USER).map(({ connectionId, info }) => {
+          // let alreadyThere: (typeof id)[] = [];
+          // if (alreadyThere.includes(id)) {
+          //   return null;
+          // }
+          // alreadyThere.push(id);
+
+          return (
+            <UserAvatar
+              borderColor={connectionIdToColor(connectionId)}
+              key={connectionId}
+              src={info?.picture}
+              name={info?.name}
+              fallback={info?.name?.[0] || "A"}
+            />
+          );
+        })}
+        {currentUser && (
+          <UserAvatar
+            borderColor={connectionIdToColor(currentUser.connectionId)}
+            src={currentUser?.info?.picture}
+            name={currentUser?.info?.name}
+            fallback={currentUser?.info?.name?.[0] || "A"}
+          />
+        )}
+
+        {hasMoreUsers && (
+          <UserAvatar
+            name={`${users.length - MAX_SHOWN_USER} more`}
+            fallback={`+${users.length - MAX_SHOWN_USER}`}
+          />
+        )}
+      </div>
     </div>
   );
 };
